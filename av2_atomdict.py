c2b601962744d452f5cb22aef995bbf8d2620836
grapesugar <shawnsgu@gmail.com>
2017-01-26 00:44:24 -0500

    Initial commit

---
+++ av2_atomdict.py
@@ -0,0 +1,20 @@
+class atom_dictionary:
+    ATM = {}
+
+    ATM["h"] = 1;
+    ATM["c.3"] = 2; ATM["c.cat"] = 2; ATM["si"] = 2;
+    ATM["c.2"] = 3; ATM["c.1"] = 3;
+    ATM["c.ar"] = 4;
+    ATM["n.4"] = 5; ATM["n.3"] = 5;
+    ATM["n.2"] = 6; ATM["n.1"] = 6; ATM["n.am"] = 6;
+    ATM["n.ar"] = 7;
+    ATM["n.pl3"] = 8;
+    ATM["o.3"] = 9;
+    ATM["o.2"] = 10;
+    ATM["o.co2"] = 11;
+    ATM["f"] = 12; ATM["cl"] = 12; ATM["br"] = 12; ATM["i"] = 12;
+    ATM["s.3"] = 13; ATM["s.2"] = 13; ATM["s.o"] = 13; ATM["s.o2"] = 13; ATM["se"] = 13;
+    ATM["p"] = 14; ATM["as"] = 14;
+    ATM["b"] = 15; ATM["mg"] = 15; ATM["zn"] = 15; ATM["fe"] = 15; ATM["v"] = 15;
+    ATM["sg"] = 15; ATM["ni"] = 15; ATM["co"] = 15; ATM["ru"] = 15; ATM["mn"] = 15;
+    ATM["mo"] = 15; ATM["re"] = 15; ATM["xx"] = 15;
